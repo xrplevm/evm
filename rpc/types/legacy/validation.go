510a039a880a3451f63148b8a5d1df1b40737889
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-12-08 11:31:14 +0100

    feat(rpc): add legacy evmos types

---
+++ rpc/types/legacy/validation.go
@@ -0,0 +1,20 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+package types
+
+import (
+	errorsmod "cosmossdk.io/errors"
+	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// ValidateAddress returns an error if the provided string is either not a hex formatted string address
+func ValidateAddress(address string) error {
+	if !common.IsHexAddress(address) {
+		return errorsmod.Wrapf(
+			errortypes.ErrInvalidAddress, "address '%s' is not a valid ethereum hex address",
+			address,
+		)
+	}
+	return nil
+}
